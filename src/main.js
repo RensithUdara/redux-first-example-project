68c09503a489ba49193415be7a2b31499daac259
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-27 23:39:39 +0530

    implement function

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { createStore } from 'redux'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const initialState = {
+  noOfFruits: 5
+}
 
-setupCounter(document.querySelector('#counter'))
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'BUY_FRUIT':
+      return {
+        ...state,
+        noOfFruits: state.noOfFruits - 1
+      }
+  }
+}
+
+const store = createStore(reducer);
+
+state.dispatch({
+  type: 'BUY_FRUIT',
+  payload: "Extra Information"
+});
